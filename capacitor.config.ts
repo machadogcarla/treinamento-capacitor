8f4ed47cd68c606612d76a30fb0128e48381c3f2
Carla Paiano <machadogcarla@gmail.com>
2022-01-27 11:53:46 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.dag.treinamento',
+  appName: 'dag-treinamento',
+  webDir: 'dist/dag-treinamento',
+  bundledWebRuntime: false
+  // server: {
+  //   url: 'http://192.168.206.999:4200',
+  //   cleartext: true
+  // }
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { Device } from '@capacitor/device';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'dag-treinamento';
+  latitude: any;
+  longitude: any;
+  bateria: any;
+  sistemaOperacional: string = '';
+  nomeDisp: any = '';
+  versaoSo: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.locate();
+    this.deviceInfo();
+  }
+
+  async locate() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.latitude = coordinates.coords.latitude;
+    this.longitude = coordinates.coords.longitude;
+  }
+
+  async deviceInfo(){
+    const info = await Device.getInfo();
+    this.sistemaOperacional = info.operatingSystem;
+    this.nomeDisp = info.name;
+    this.versaoSo = info.osVersion;
+
+    const bateria = await Device.getBatteryInfo();
+    this.bateria = bateria.batteryLevel;
+  }
+
 }
+
+
+
+
